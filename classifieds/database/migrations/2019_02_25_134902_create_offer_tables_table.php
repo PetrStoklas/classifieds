e85ffa519d66303952b1fc55bcdd1c7f741679bb
Petr Stoklas <petrstoklas@gmail.com>
2019-02-25 15:03:01 +0100

    offer table migration and model

---
+++ classifieds/database/migrations/2019_02_25_134902_create_offer_tables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOfferTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offer_tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('expired')->default(false);
+            $table->integer('product_id')->default(1);
+            $table->integer('category_id')->default(1);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offer_tables');
+    }
+}
