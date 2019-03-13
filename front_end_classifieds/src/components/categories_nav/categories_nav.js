7b842336433e924cbcf0e857b44d32a203d1a0d3
Igor <igor_pavlenko@yahoo.com>
2019-03-13 10:09:59 +0100

    updates on categories_nav

--- front_end_classifieds/src/components/categories_nav/categories_nav.js
+++ front_end_classifieds/src/components/categories_nav/categories_nav.js
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 // import { Spinner } from 'reactstrap';
 
 const Categories_nav = props => {
+    console.log(props);
     let categories = '';
 
 
+        let categoriesType = '';
+
         if(props.categories) {
-            categories = props.categories.map(res => 
+            categoriesType = props.categories;
+        } else if(props.subCats) {
+            categoriesType = props.subCats
+        }
+        
+            categories = categoriesType.map(res => 
                 <ListGroupItem key={res.id} onClick={props.getSubcategories}>
                     <Link 
                           to={"/" + res.name} id={res.id}>{res.name}
                     </Link>
                 </ListGroupItem>)
-        }  
-        if(props.subCats) {
-            categories = props.subCats.map(res => 
-                <ListGroupItem key={res.id}>
-                    <Link 
-                          to={"/" + res.id} id={res.id}>{res.name}
-                    </Link>
-                </ListGroupItem>)
-        }
+        
     return (
       <ListGroup flush>
         {categories}
