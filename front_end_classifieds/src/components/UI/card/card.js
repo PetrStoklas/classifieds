e119821d87f34ec871636d230d7052dee28017ee
Petr Stoklas <petrstoklas@gmail.com>
2019-03-21 16:55:51 +0100

    commit for igor

--- front_end_classifieds/src/components/UI/card/card.js
+++ front_end_classifieds/src/components/UI/card/card.js
@@ -37,12 +37,12 @@ const Example = (props) => {
             <Col xs="6 left">
               <Row className="card_title"><span>Dodge Challenger</span></Row>
               <Row className="values_row">
-                <Col class="values_col "><h4>120000km</h4></Col>
-                <Col class="values_col right"><h4>gasoline</h4></Col>
+                <Col className="values_col "><h4>120000km</h4></Col>
+                <Col className="values_col right"><h4>gasoline</h4></Col>
               </Row>
               <Row className="values_row">
-                <Col class="values_col"><h4>2.8L Injection</h4></Col>
-                <Col class="values_col right"><h4>manual</h4></Col>     
+                <Col className="values_col"><h4>2.8L Injection</h4></Col>
+                <Col className="values_col right"><h4>manual</h4></Col>     
               </Row>
             </Col>
             
@@ -54,9 +54,9 @@ const Example = (props) => {
 
              <Row className="location_price_row">
 
-             <Col class="values_col location"><h4 className="location">Prague, CZ</h4></Col>
+             <Col className="values_col location"><h4 className="location">Prague, CZ</h4></Col>
 
-             <Col class="values_col price"><h4 className="price">1500000<span className="currency">czk</span></h4></Col>
+             <Col className="values_col price"><h4 className="price">1500000<span className="currency">czk</span></h4></Col>
 
              </Row>
             </Col>
--- front_end_classifieds/src/components/product_card/product_card_detail.jsx
+++ front_end_classifieds/src/components/product_card/product_card_detail.jsx
@@ -1,106 +1,107 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap'; 
+import {Container, Row, Col, Card, Button} from 'reactstrap'; 
+import ProductCarousel from '../UI/ProductCarousel/ProductCarousel';
 import './product_card_detail.css';
 
 const card_detail = props => {
     return (
-        // <Col md="6" lg="4">
-        // <Card body className="w-100">
-        //   <Row class="product-image">
-        //   <div class="product-image">
-        //     <ProductCarousel />
-        //   </div>
-        //   </Row>
+        <Col md="6" lg="4">
+        <Card body className="w-100">
+          <Row class="product-image">
+          <div class="product-image">
+            <ProductCarousel />
+          </div>
+          </Row>
 
-        //   <Row className="card_info_section">
-        //     <Container className="car_info rounded mt-2 mx-2 px-1">
-        //       <hr/>
-        //       <div className="title"><h3>{props.name}</h3></div>
+          <Row className="card_info_section">
+            <Container className="car_info rounded mt-2 mx-2 px-1">
+              <hr/>
+              <div className="title"><h3>{props.name}</h3></div>
 
-        //       <div className="info_wrap">
-        //       <Row className="row_info mx-auto mt-0">
-        //         <Col className="car_info_col first" xs="4" >
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //         <Col className="car_info_col middle first" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //           </Col>
-        //         <Col className="car_info_col first" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000</h4>
-        //         </Col>
-        //       </Row>
+              <div className="info_wrap">
+              <Row className="row_info mx-auto mt-0">
+                <Col className="car_info_col first" xs="4" >
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+                <Col className="car_info_col middle first" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                  </Col>
+                <Col className="car_info_col first" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000</h4>
+                </Col>
+              </Row>
 
-        //       <Row className="row_info mx-auto">
-        //       <Col className="car_info_col" xs="4" >
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //         <Col className="car_info_col middle" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //           </Col>
-        //         <Col className="car_info_col" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //       </Row>
+              <Row className="row_info mx-auto">
+              <Col className="car_info_col" xs="4" >
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+                <Col className="car_info_col middle" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                  </Col>
+                <Col className="car_info_col" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+              </Row>
 
-        //       <Row className="row_info mx-auto">
-        //         <Col className="car_info_col" xs="4" >
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //         <Col className="car_info_col middle" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //           </Col>
-        //         <Col className="car_info_col" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //       </Row>
+              <Row className="row_info mx-auto">
+                <Col className="car_info_col" xs="4" >
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+                <Col className="car_info_col middle" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                  </Col>
+                <Col className="car_info_col" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+              </Row>
 
-        //       <Row className="row_info mx-auto">
-        //         <Col className="car_info_col" xs="4" >
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //         <Col className="car_info_col middle" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //           </Col>
-        //         <Col className="car_info_col" xs="4">
-        //           <h5 className="car_info_headline">mileage</h5>
-        //           <h4 className="car_info_value">120000km</h4>
-        //         </Col>
-        //       </Row>
-        //       </div>
+              <Row className="row_info mx-auto">
+                <Col className="car_info_col" xs="4" >
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+                <Col className="car_info_col middle" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                  </Col>
+                <Col className="car_info_col" xs="4">
+                  <h5 className="car_info_headline">mileage</h5>
+                  <h4 className="car_info_value">120000km</h4>
+                </Col>
+              </Row>
+              </div>
 
               
-        //       <div className="custom_desc">
-        //         <p>Custom added AC, sport brakes Brembo, Recaro seats. The car was never crashed. I am the third owner. Can provide you with all documentation and paperworks of this car.
-        //         </p>
-        //       </div>
+              <div className="custom_desc">
+                <p>Custom added AC, sport brakes Brembo, Recaro seats. The car was never crashed. I am the third owner. Can provide you with all documentation and paperworks of this car.
+                </p>
+              </div>
               
               
 
 
            
-    //         </Container>
+            </Container>
 
-    //       </Row>
+          </Row>
 
 
        
        
-    //   </Card>
-    //   <Button className="buy-btn">Button</Button>
-    //   </Col>
+      </Card>
+      <Button className="buy-btn">Button</Button>
+      </Col>
             
-        <h1>product card</h1>
+        // <h1>product card</h1>
     )
 }
 
