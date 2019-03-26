1034e4be4d77f1d7b33345717b911828a980fa28
Igor <igor_pavlenko@yahoo.com>
2019-03-26 20:37:13 +0200

    added all the brands to the brand cards

--- front_end_classifieds/src/components/brandCard/brandCard.js
+++ front_end_classifieds/src/components/brandCard/brandCard.js
@@ -9,6 +9,17 @@ const BrandCard = props => {
     BMW: 'bmw_new.png',
     MERCEDES: 'Mercedes-Benz.png',
     VOLVO: 'volvo-logo.png',
+    AUDI: 'audi-logo.png',
+    FORD: 'ford-logo.png',
+    HUNDAI: 'hundai-logo.png',
+    KIA: 'kia-logo.png',
+    CITROEN: 'citroen-logo.png',
+    VOLVKSWAGEN: 'vw-logo.png',
+    SKODA: 'skoda-logo.png',
+    DAEWOO: 'daewoo-logo.png',
+    SUBARU: 'subaru-logo.png',
+    TOYOTA: 'toyota-logo.png',
+    CHERRY: 'cherry-logo.png'
   }
 
   let brandCard = '';
