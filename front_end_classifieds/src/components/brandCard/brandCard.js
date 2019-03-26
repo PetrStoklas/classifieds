731bd468b4194b2cbb83cc86a7718a17a5b32979
Igor <igor_pavlenko@yahoo.com>
2019-03-26 19:40:24 +0200

    redering first icon test

--- front_end_classifieds/src/components/brandCard/brandCard.js
+++ front_end_classifieds/src/components/brandCard/brandCard.js
@@ -3,21 +3,27 @@ import React from 'react';
 import styles from './brandCard.module.css';
 
 const BrandCard = props => {
-  
+
+  const images = {
+    HONDA: 'honda-logo.jpg'
+  }
+
   let brandCard = '';
   brandCard = props
     .categories
     .categories
     .map(res => (
-      <div className={styles.Card}
-      key={res.id}
-      id={res.id}
-      
-      onClick={() => { 
+      <div
+        className={styles.Card}
+        key={res.id}
+        id={res.id}
+        onClick={() => {
         props.getChildrenId(res.id)
-      }}
-      
-      >{res.name}</div>
+      }}>
+        <img
+          className={styles.BrandLogo}
+          src={'http://127.0.0.1:8000/uploads/brand_icons/' + images[res.name]}/>
+      </div>
     ))
 
   return (
