efbd2525efe4294f8ceb507e7558d4306fa43e12
Igor <igor_pavlenko@yahoo.com>
2019-03-26 20:46:50 +0200

    separated the brand cards into config file

--- front_end_classifieds/src/components/brandCard/brandCard.js
+++ front_end_classifieds/src/components/brandCard/brandCard.js
@@ -1,26 +1,11 @@
 import React from 'react';
 // eslint-disable-next-line
 import styles from './brandCard.module.css';
+import images from '../../config_files/brandCards';
 
 const BrandCard = props => {
 
-  const images = {
-    HONDA: 'honda-logo.png',
-    BMW: 'bmw_new.png',
-    MERCEDES: 'Mercedes-Benz.png',
-    VOLVO: 'volvo-logo.png',
-    AUDI: 'audi-logo.png',
-    FORD: 'ford-logo.png',
-    HUNDAI: 'hundai-logo.png',
-    KIA: 'kia-logo.png',
-    CITROEN: 'citroen-logo.png',
-    VOLVKSWAGEN: 'vw-logo.png',
-    SKODA: 'skoda-logo.png',
-    DAEWOO: 'daewoo-logo.png',
-    SUBARU: 'subaru-logo.png',
-    TOYOTA: 'toyota-logo.png',
-    CHERRY: 'cherry-logo.png'
-  }
+  console.log(images);
 
   let brandCard = '';
   brandCard = props
---
+++ front_end_classifieds/src/config_files/brandCards.js
@@ -0,0 +1,19 @@
+const images = {
+  HONDA: 'honda-logo.png',
+  BMW: 'bmw_new.png',
+  MERCEDES: 'Mercedes-Benz.png',
+  VOLVO: 'volvo-logo.png',
+  AUDI: 'audi-logo.png',
+  FORD: 'ford-logo.png',
+  HUNDAI: 'hundai-logo.png',
+  KIA: 'kia-logo.png',
+  CITROEN: 'citroen-logo.png',
+  VOLVKSWAGEN: 'vw-logo.png',
+  SKODA: 'skoda-logo.png',
+  DAEWOO: 'daewoo-logo.png',
+  SUBARU: 'subaru-logo.png',
+  TOYOTA: 'toyota-logo.png',
+  CHERRY: 'cherry-logo.png'
+}
+
+export default images;
