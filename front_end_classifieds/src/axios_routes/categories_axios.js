d81041036367b2dda84997a22e7069da0050116e
Igor <igor_pavlenko@yahoo.com>
2019-03-31 10:40:12 +0300

    fixed the CORS issue now we can stop using the chrome extension

--- front_end_classifieds/src/axios_routes/categories_axios.js
+++ front_end_classifieds/src/axios_routes/categories_axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/"; 
 
 const categories = axios.create({
     
-    baseURL: 'http://www.api.testweb.life/api/categories',
+  baseURL: proxyurl + 'http://www.api.testweb.life/api/categories',
     timeout: 6000,
     // mode: 'no-cors',
     headers: {'X-Custom-Header': 'foobar'}
--- front_end_classifieds/src/axios_routes/products_axios.js
+++ front_end_classifieds/src/axios_routes/products_axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/"; 
 
 const fetchProducts = axios.create({
-    baseURL: 'http://www.api.testweb.life/api/products',
+    baseURL: proxyurl + 'http://www.api.testweb.life/api/products',
     timeout: 5000,
     headers: {
         'X-Custom-Header': 'foobar'
