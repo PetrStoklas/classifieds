b049758e72ae19716e2f6635c0cc902bfecd3477
Igor <igor_pavlenko@yahoo.com>
2019-04-04 14:52:54 +0300

    jumbotron folder name change

---
+++ front_end_classifieds/src/components/categoriesJumbotr/categoriesJumbotr.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Jumbotron, 
+    // Button 
+} from 'reactstrap';
+import classes from './header.module.css';
+import BrandCard from '../brandCard/brandCard';
+
+const header = props => {
+
+  return (
+    <div>
+    <Jumbotron className={[classes.Wrapper, classes.Background]}>
+        <h1 className={classes.Header +" display-3"}>......</h1>
+        <p className="lead">Best Automotive Trading Platform in Ukraine</p>
+        <hr className="my-2" />
+        <BrandCard 
+          categories={props}
+          getChildrenId={(id) => { props.getCategoryId(id)}}
+        />
+        <p className="lead">
+        </p>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default header
